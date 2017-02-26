a13f64929d6a989b6c287725e02366a53b2c8ddf
Caublecm <caublecm@gmail.com>
2017-02-26 14:25:01 -0800

    adding hw7.py

--- hw7.py
+++ hw7.py
@@ -1,123 +1,77 @@
+from sortedcontainers import SortedDict
 
-from collections import OrderedDict
+def print_menu():
+    print('1. Print Users')
+    print('2. Add a User')
+    print('3. Remove a User')
+    print('4. Lookup a Username')
+    print('5. Quit')
+    print()
 
-#making edits
-#lalalala
+# Create dictionary with key = Names, value = user_name
+usernames = SortedDict()
+usernames['Summer'] = 'summerela'
+usernames['William'] = 'GoofyFish'
+usernames['Steven'] = 'LoLCat'
+usernames['Zara'] = 'zanyZara'
+usernames['Renato'] = 'songDude'
 
+# setup counter to store menu choice
+menu_choice = 0
 
-print( 'Regular dictionary')
-d = {}
-d['a'] = 'A'
-d['b'] = 'B'
-d['c'] = 'C'
-d['d'] = 'D'
-d['e'] = 'E'
+#display your menu
+print_menu()
 
-for k, v in d.items():
-    print (k, v)
+# as long as the menu choice isn't "quit" get user options
+while menu_choice != 5:
+    # get menu choice from user
+    # use try/except to insure a integer number is entered
+    try:
+        menu_choice = int(input("Type in a number (1-5): "))
+    except ValueError:
+        print("That is not a number!\n".format(menu_choice))
+    
+    # view current entries
+    if menu_choice == 1:
+        print("Current Users:")
+        for x,y in usernames.items():
+            print("Name: {} \tUser Name: {} \n".format(x,y))
+            
+    # add an entry
+    elif menu_choice == 2:
+        print("Add User")
+        name = input("Name: ")
+        username = input("User Name: ")
+        usernames[name] = username
+      
+        usernames[name] = username
+        
+    # remove an entry
+    elif menu_choice == 3:
+        print("Remove User")
+        name = input("Name: ")
+        if name in usernames:
+            #if key exists in dictionary delete the key and value for that entry
+            del usernames[name]
+            #inform user the entry has been deleted
+            print("Name: {} deleted\n".format(name))
+        else:
+            # if key does not exist state "not found"
+            print("Name: {} not found\n".format(name))
 
-print('\nOrderedDict:')
-d = OrderedDict()
-d['a'] = 'A'
-d['b'] = 'B'
-d['c'] = 'C'
-d['d'] = 'D'
-d['e'] = 'E'
-
-for k, v in d.items():
-    print(k, v)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from sortedcontainers import SortedDict
-#
-#
-# def print_menu():
-#     print('1. Print Users')
-#     print('2. Add a User')
-#     print('3. Remove a User')
-#     print('4. Lookup a Phone Number')
-#     print('5. Quit')
-#     print()
-#
-#
-# # Create dictionary with key = Names, value = user_name
-# usernames = SortedDict()
-# usernames['Summer'] = 'summerela'
-# usernames['William'] = 'GoofyFish'
-# usernames['Steven'] = 'LoLCat'
-# usernames['Zara'] = 'zanyZara'
-# usernames['Renato'] = 'songDude'
-#
-# # setup counter to store menu choice
-# menu_choice = 0
-#
-# # display your menu
-# print_menu()
-#
-# # as long as the menu choice isn't "quit" get user options
-# while menu_choice != 5:
-#     # get menu choice from user
-#     menu_choice = int(input("Type in a number (1-5): "))
-#
-#     # view current entries
-#     if menu_choice == 1:
-#         print("Current Users:")
-#         for x, y in usernames.items():
-#             print("Name: {} \tUser Name: {} \n".format(x, y))
-#
-#     # add an entry
-#     elif menu_choice == 2:
-#         print("Add User")
-#         name = input("Name: ")
-#         username = input("User Name: ")
-#         usernames[name] = username
-#
-#     # remove an entry
-#     elif menu_choice == 3:
-#         print("Remove User")
-#         name = input("Name: ")
-#         if name in usernames:
-#             pass  # delete that entry
-#
-#     # view user name
-#     elif menu_choice == 4:
-#         print("Lookup User")
-#         name = input("Name: ")
-#         if name in usernames:
-#             pass  # print the username
-#         else:
-#             pass  # print username not found
-#
-#     # is user enters something strange, show them the menu
-#     elif menu_choice != 5:
-#         print_menu()
+    # view user name      
+    elif menu_choice == 4:
+        print("Lookup Username")
+        name = input("Name: ")
+        if name in usernames:
+            #if key exists in dictionary delete the key and value for that entry
+            username = usernames[name]
+            #inform user the entry has been deleted
+            print("Name: {}\tUserName: {} \n".format(name, username))
+        else:
+            # if key does not exist state "not found"
+            print("Name: {} not found\n".format(name))
+    
+    # is user enters something strange, show them the menu
+    elif menu_choice != 5:
+        print_menu()
